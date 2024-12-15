1352a926dc3558907ac39c1a975f16aaf40a2f6c
felixsetio88 <128802561+felixsetio88@users.noreply.github.com>
2024-12-16 01:37:30 +0800

    Added "Do you know" card

--- app/components/dashboard/Recommendations.tsx
+++ app/components/dashboard/Recommendations.tsx
@@ -1,26 +1,42 @@
 'use client';
 
 import { Card } from '@/components/ui/card';
-import { Leaf, Bus, Lightbulb } from 'lucide-react';
+import { Leaf, Bus, Lightbulb, Zap, Car, Construction} from 'lucide-react';
 
 export function Recommendations() {
   return (
-    <Card className="p-4">
-      <h3 className="font-semibold mb-4">Personalized Recommendations</h3>
+    <><Card className="p-4">
+      <h3 className="font-semibold mb-4">Do you know?</h3>
       <ul className="space-y-4">
         <li className="flex items-start space-x-3">
-          <Bus className="h-5 w-5 text-green-500 mt-0.5" />
-          <p>Consider using public transportation more frequently to reduce your transportation emissions.</p>
+          <Zap className="h-5 w-5 text-green-500 mt-0.5" />
+          <p>Electricity and heat cause 29.7% of emissions.</p>
         </li>
         <li className="flex items-start space-x-3">
-          <Lightbulb className="h-5 w-5 text-green-500 mt-0.5" />
-          <p>Switch to LED bulbs to reduce your energy consumption.</p>
+          <Car className="h-5 w-5 text-green-500 mt-0.5" />
+          <p>Transportation cause 13.7% of emission.</p>
         </li>
         <li className="flex items-start space-x-3">
-          <Leaf className="h-5 w-5 text-green-500 mt-0.5" />
-          <p>Try incorporating more plant-based meals into your diet.</p>
+          <Construction className="h-5 w-5 text-green-500 mt-0.5" />
+          <p>manufacturing and construction cause 12.7% of emission.</p>
         </li>
       </ul>
-    </Card>
+    </Card><Card className="mt-5 p-4">
+        <h3 className="font-semibold mb-4">Personalized Recommendations</h3>
+        <ul className="space-y-4">
+          <li className="flex items-start space-x-3">
+            <Bus className="h-5 w-5 text-green-500 mt-0.5" />
+            <p>Consider using public transportation more frequently to reduce your transportation emissions.</p>
+          </li>
+          <li className="flex items-start space-x-3">
+            <Lightbulb className="h-5 w-5 text-green-500 mt-0.5" />
+            <p>Switch to LED bulbs to reduce your energy consumption.</p>
+          </li>
+          <li className="flex items-start space-x-3">
+            <Leaf className="h-5 w-5 text-green-500 mt-0.5" />
+            <p>Try incorporating more plant-based meals into your diet.</p>
+          </li>
+        </ul>
+      </Card></>
   );
 }
